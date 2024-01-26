6f8a308ed11f74ab44c727c482d785c90692ab22
eddie.chang <eddie605134@gmail.com>
2024-01-26 11:53:57 +0800

    day_2

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
-// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-// async fn greet() -> impl Responder {
-//     let mut client = "Hello world!";
-//     HttpResponse::Ok().body(client)
-// }
-
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .route("/", web::get().to(greet))
-//     })
-//     .bind("127.0.0.1:8080")?
-//     .run()
-//     .await
-// }
-
 fn main() {
-    println!("Hello rust!");
+    // let age: i32 = 20;
+    let mut age: i32 = 20;
+    // age = 3.14;
+    println!("Hello i'm {}", age);
+
+    //  分成 i isize ; u usize
+    let age_i8: i8 = 20;
+
+    let books: u32 = 1_000_00_0;
+
+    // 浮點數
 }
