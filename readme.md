94724cd29ee3d7b6941361d591cc09ab86bbbe5e
eddie.chang <eddie605134@gmail.com>
2024-01-26 11:09:28 +0800

    day_1

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+## 參考資料
+* [為你自己學 Rust](https://ithelp.ithome.com.tw/users/20065770/ironman/6174)
+* [Rust 程式設計語言](https://rust-lang.tw/book-tw/ch00-00-introduction.html)
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet() -> impl Responder {
-    let mut client = "Hello world!";
-    HttpResponse::Ok().body(client)
-}
+// async fn greet() -> impl Responder {
+//     let mut client = "Hello world!";
+//     HttpResponse::Ok().body(client)
+// }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/", web::get().to(greet))
+//     })
+//     .bind("127.0.0.1:8080")?
+//     .run()
+//     .await
+// }
+
+fn main() {
+    println!("Hello rust!");
 }
