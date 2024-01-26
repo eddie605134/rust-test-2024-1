d069cf287593af942b8e39d124fb4a3bb884a45a
eddie.chang <eddie605134@gmail.com>
2024-01-26 12:10:42 +0800

    day_4

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 fn main() {
-    let message = 'w';
-    println!("Hello,{}", message);
+    // u8 -> 型別; 3 -> 長度
+    let list: [u8; 3] = [1, 2, 3];
 
-    let cc: char = 'a';
-    let huh = '蛤';
-    let cat = '🐈';
+    println!("{:?}", list);
 
-    println!("{} says {}", cat, huh);
+    // let list2 = [2, "3", 4];
 
-    let happy: bool = true;
-    let is_adult = false;
+    let list3 = [1450, 9527, 5566];
 
-    let cats: u8 = 0;
-
-    if cats > 0 {
-        println!("好多貓");
-    } else {
-        println!("一隻貓");
+    for item in list3.iter() {
+        println!("{}", item);
     }
+
+    let list4 = [1450, 9527, 5566];
+    let [_, b, c] = list4;
+
+    // tuple
+    let answer: (char, bool) = ('蛤', false);
+    let pet = ('🐈', false);
+
+    let point = (100, 200, 300);
+    let (x, y, z) = point;
+
+    // Unit () 空tuple; 類似 void
+    // 表示「這個函數是沒有回傳值」
+
+    // fn main() -> () {
+    //     println!("Hello Rust")
+    // }
 }
